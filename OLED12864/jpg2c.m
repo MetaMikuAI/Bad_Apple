35c7666c69ff88d933e2e104928e51e6cbe66851
【MATLAB】absent <46769316+huixiangli@users.noreply.github.com>
2020-07-31 10:11:14 +0800

    Update jpg2c.m

--- OLED12864/jpg2c.m
+++ OLED12864/jpg2c.m
@@ -1,8 +1,8 @@
 fp=fopen('name.c','wt');
-fprintf(fp,['int data[',num2str(1024*LENGTH),']={']);
 
 START=1;STEP=3;END=6564;
 LENGTH=length(START:STEP:END);
+fprintf(fp,['int data[',num2str(1024*LENGTH),']={']);
 K='';
 
 for f=START:STEP:END
@@ -10,10 +10,13 @@ for f=START:STEP:END
     fprintf('%d\n',f);
     for m=0:7   %8行（8 pages）
         for n=0:127   %128列（128 Co）
+
             K=bin2dec( num2str( rot90( im_( 8*m+1:8*m+8,n+1 ) ,3) ) );
             fprintf(fp,[num2str(K),',']);%WARNING:the last ',' is unnecessary,it should be deleted by yourself!
+        
         end
     end
 end
+
 fprintf(fp,'};');
 fclose(fp);
